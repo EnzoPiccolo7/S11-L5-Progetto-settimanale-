39e3e099999c56c1a5a13dc9a8cf3d53f145d8fc
Enzo <EnzoPiccolo7>
2022-07-09 17:34:18 +0200

    primo commit

---
+++ Angular/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
+import { HomeModule } from './home/home.module';
+import { AuthModule } from './auth/auth.module';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {  MatMenuModule} from '@angular/material/menu'; 
+import {MatButtonModule} from '@angular/material/button'; 
+/* import { UsersModule } from './users/users.module'; */
+/* import { Err404Module } from './err404/err404.module'; */
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeaderModule,
+    HomeModule,
+    AuthModule,
+    MatSliderModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule
+    /* UsersModule, */
+    /* Err404Module */
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  
+ }
---
+++ Angular/src/app/header/header.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavComponent } from './nav/nav.component';
+import { CoreModule } from '../core/core.module';
+import { AppRoutingModule } from '../app-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatIconModule} from '@angular/material/icon';
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    NavComponent
+  ],
+  imports: [
+    CommonModule,
+    CoreModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule
+    
+  ],
+  exports: [
+    NavbarComponent,
+    NavComponent
+  ]
+})
+export class HeaderModule { }
